663b6a38dad541f80094e220e6ac706036b3ec1e
unknown <yenbi2609@gmail.com>
2021-09-20 22:47:41 +0700

    commit

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+
+module.exports = function () {
+  this.Given(/^I visit google$/, function() {
+    return this.driver.get('http://www.google.com');
+  });
+
+  this.Then(/^I should see Google in title$/, function() {
+    this.driver.getTitle().then(function (title) {
+      assert.equal(title, "Google");
+      console.log(title);
+      return title;
+    });
+  });
+};
